5d056754df50f7c4d23fa7d8e3c034ae5973ec88
Matheus Vinicius Moraes Bernardino <matheusvmb@educ.guarulhos.gov>
2025-04-29 14:28:48 -0300

    Created -> PaginaCarregando.vue e suas estilizações e as transições do ProdutoLista.vue

--- src/router.js
+++ src/router.js
@@ -1,17 +1,21 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
-    }
-  ]
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+  ],
+  scrollBehavior() {
+    // Função para rolar a página para o topo ao mudar de rota
+    return window.scrollTo({ top: 0, behavior: 'smooth' });
+  },
 });
