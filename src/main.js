0cac2eacfcabd225a6c866ed2a05f97597e8e062
Matheus Vinicius Moraes Bernardino <matheusvmb@educ.guarulhos.gov>
2025-04-29 15:35:07 -0300

    Created -> Produto.vue e filtro de preço em BRL

--- src/main.js
+++ src/main.js
@@ -6,7 +6,21 @@ import PaginaCarregando from '@/components/PaginaCarregando.vue';
 
 Vue.config.productionTip = false;
 
+// Componente para exibir uma mensagem de carregamento
 Vue.component('PaginaCarregando', PaginaCarregando);
+
+// Filtro para formatar o preço do produto
+Vue.filter('numeroPreco', (valor) => {
+  valor = Number(valor);
+  if (!isNaN(valor)) {
+    return valor.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+  } else {
+    return '';
+  }
+});
 
 new Vue({
   router,
