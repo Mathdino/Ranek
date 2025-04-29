dcacc1d7a6494e16156a1f381f96d0b552a3506a
Matheus Bernardino <123327011+Mathdino@users.noreply.github.com>
2025-04-29 10:28:44 -0300

    Resolvendo alguns bug

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import {api} from '@/service.js';
+import { serialize } from '@/helpers.js';
+
+export default {
+  name: 'ProdutosLista',
+  data() {
+    return {
+      produtos: null,
+      produtosPorPagina: 9,
+      produtosTotal: 0,
+    };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    },
+  },
+  methods: {
+    getProdutos() {
+      this.produtos = null;
+      api.get(this.url).then((response) => {
+        this.produtosTotal = Number(response.headers['x-total-count']);
+        this.produtos = response.data;
+      });
+    },
+  },
+  watch: {
+    url() {
+      this.getProdutos();
+    },
+  },
+  created() {
+    this.getProdutos();
+  },
+};
+</script>
+
+<style></style>
