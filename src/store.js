cfadaab3966393e900a624e801e692fab9dcb557
Matheus Vinicius Moraes Bernardino <matheusvmb@educ.guarulhos.gov>
2025-04-30 13:17:12 -0300

    ATT ->Plugin próprio para realizar o envio do form até a store.js

--- src/store.js
+++ src/store.js
@@ -1,41 +1,40 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import { api } from "@/service.js";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { api } from '@/service.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
     login: false,
     usuario: {
-      id: "",
-      nome: "",
-      email:"",
-      senha:"",
-      cep:"",
-      rua:"",
-      numero:"",
-      bairro:"",
-      cidade:"",
-      estado:"",
-    }
+      id: '',
+      nome: '',
+      email: '',
+      senha: '',
+      cep: '',
+      rua: '',
+      numero: '',
+      bairro: '',
+      cidade: '',
+      estado: '',
+    },
   },
   mutations: {
-    UPDATE_LOGIN(state, payload){
+    UPDATE_LOGIN(state, payload) {
       state.login = payload;
     },
-    UPDATE_USUARIO(state, payload){
-      state.usuario = payload;
+    UPDATE_USUARIO(state, payload) {
+      state.usuario = Object.assign(state.usuariom, payload);
     },
-
   },
   actions: {
-    getUsuario(context, payload){
-      api.get(`/usuario/${payload}`).then(response => {
-        context.commit("UPDATE_USUARIO", response.data)
-        context.commit("UPDATE_LOGIN", true )
-
-      })
-    }
-  }
+    getUsuario(context, payload) {
+      api.get(`/usuario/${payload}`).then((response) => {
+        context.commit('UPDATE_USUARIO', response.data);
+        context.commit('UPDATE_LOGIN', true);
+      });
+    },
+  },
 });
