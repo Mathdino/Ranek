e0dcb62f629d23ffbceb72b96377f4743dcd3dda
Matheus Bernardino <123327011+Mathdino@users.noreply.github.com>
2025-05-05 21:19:52 -0300

    ATT -> Função de atualizar cadastro

--- src/views/usuario/UsuarioEditar.vue
+++ src/views/usuario/UsuarioEditar.vue
@@ -1,9 +1,46 @@
 <template>
-  <p>Usuario Editar</p>
+  <section>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">Atualizar Usuário</button>
+    </UsuarioForm>
+    <ErroNotificacao :erros="erros"/>
+  </section>
 </template>
 
 <script>
-export default {};
+import UsuarioForm from "@/components/UsuarioForm.vue";
+import { api } from "@/services.js";
+
+export default {
+  name: "UsuarioEditar",
+  components: {
+    UsuarioForm
+  },
+  data() {
+    return {
+      erros: []
+    };
+  },
+  methods: {
+    atualizarUsuario() {
+      this.erros = [];
+      api
+        .put("/usuario", this.$store.state.usuario)
+        .then(() => {
+          this.$store.dispatch("getUsuario");
+          this.$router.push({ name: "usuario" });
+        })
+        .catch(error => {
+          this.erros.push(error.response.data.message);
+        });
+    }
+  },
+  created() {
+    document.title = "Usuário | Editar";
+  }
+};
 </script>
 
-<style></style>
+<style>
+</style>
+
